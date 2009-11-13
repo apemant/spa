40bc49187d844c49ae3ff3836dbcc857d4f39ec9
abagaric <>
2009-11-13 13:31:11 +0000

    - za slanje sekvence za otvaranje ladice

---
+++ devel/projects/spa/src/hr/restart/robno/repDrawer.java
@@ -0,0 +1,40 @@
+package hr.restart.robno;
+
+import java.util.StringTokenizer;
+
+import hr.restart.baza.dM;
+import hr.restart.sisfun.frmParam;
+import hr.restart.util.lookupData;
+import hr.restart.util.reports.mxRM;
+import hr.restart.util.reports.mxReport;
+
+
+public class repDrawer extends mxReport {
+
+  dM dm = dM.getDataModule();
+  lookupData lD =  lookupData.getlookupData();
+
+  public void makeReport() {
+    String mx = frmParam.getParam("sisfun", "printerRMcmnd", "1", "Radno mjesto", true);
+    lD.raLocate(dm.getMxPrinterRM(), "CRM", mx);
+    mxRM rm = new mxRM();
+    rm.init(dm.getMxPrinterRM());
+    setRM(rm);
+      try {
+        String str = frmParam.getParam("pos", "openDrawer", 
+            "\\u001B\\u0064\\u0000", "Sekvenca za otvaranje ladice", true);
+        StringTokenizer tok = new StringTokenizer(str,"\\u");
+        char[] ret = new char[tok.countTokens()];
+        int i=0;
+        while (tok.hasMoreTokens()) {
+          ret[i] = (char)Integer.parseInt(tok.nextToken(), 16);
+          i++;
+        }
+        this.setPgHeader(new String(ret));
+      } catch (NumberFormatException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }    
+    super.makeReport();
+  }
+}
