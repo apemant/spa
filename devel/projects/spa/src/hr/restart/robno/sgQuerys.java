cbf4146828e44865a7d75649df2dc8c407fce862
abagaric <>
2010-01-12 09:41:49 +0000

    - double/bigdecimal fix

--- devel/projects/spa/src/hr/restart/robno/sgQuerys.java
+++ devel/projects/spa/src/hr/restart/robno/sgQuerys.java
@@ -1340,10 +1340,10 @@ System.out.println("QS : " + queryString);
         
         stds.setBigDecimal("IRAZ", stds.getBigDecimal("IRAZ").add(ts.getBigDecimal("IRAZ")));
         stds.setBigDecimal("IPRODBP", stds.getBigDecimal("IPRODBP").add(ts.getBigDecimal("IPRODBP")));
-        stds.setBigDecimal("POR", stds.getBigDecimal("POR").add(ts.getBigDecimal("POR")));
+        stds.setBigDecimal("POR", stds.getBigDecimal("POR").add(dbl ? new BigDecimal(ts.getDouble("POR")) : ts.getBigDecimal("POR")));
         stds.setBigDecimal("IPRODSP", stds.getBigDecimal("IPRODSP").add(ts.getBigDecimal("IPRODSP")));
         stds.setBigDecimal("INAB", stds.getBigDecimal("INAB").add(ts.getBigDecimal("INAB")));
-        stds.setBigDecimal("RUC", stds.getBigDecimal("RUC").add(ts.getBigDecimal("RUC")));
+        stds.setBigDecimal("RUC", stds.getBigDecimal("RUC").add(dbl ? new BigDecimal(ts.getDouble("RUC")) : ts.getBigDecimal("RUC")));
         try {
           stds.setBigDecimal("PostoRUC",stds.getBigDecimal("RUC").divide(stds.getBigDecimal("INAB"),4,java.math.BigDecimal.ROUND_HALF_UP).multiply(new java.math.BigDecimal("100.00")));
         }
