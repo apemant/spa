57c42ee3da21293f09b4b0a5e4cc360da4d98aac
abagaric <>
2010-09-27 01:16:10 +0000

    - prva verzija

--- devel/projects/spa/src/hr/restart/robno/raEDI.java
+++ devel/projects/spa/src/hr/restart/robno/raEDI.java
@@ -6,6 +6,7 @@ import hr.restart.baza.VTCartPart;
 import hr.restart.baza.dM;
 import hr.restart.baza.doki;
 import hr.restart.baza.stdoki;
+import hr.restart.sisfun.TextFile;
 import hr.restart.sisfun.frmParam;
 import hr.restart.util.Aus;
 import hr.restart.util.lookupData;
@@ -15,6 +16,7 @@ import hr.restart.util.raTransaction;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
@@ -179,5 +181,134 @@ public class raEDI {
     }
     cal.set(cal.MILLISECOND, 0);
     return new Timestamp(cal.getTime().getTime());
+  }
+  
+  public static void importPanteon(File dir) {
+  	File pg = new File(dir, "PG.lck");
+  	if (pg.exists()) {
+  		JOptionPane.showMessageDialog(null, "Mapa je zauzeta. Probajte malo kasnije.", "Sinkronizacija", JOptionPane.WARNING_MESSAGE);
+  		return;
+  	}
+  	File sw = new File(dir, "SW.lck");
+  	try {
+			sw.createNewFile();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Greška kod sinkronizacije!", "Sinkronizacija", JOptionPane.ERROR_MESSAGE);
+		}
+  	try {
+  		if (pg.exists()) {
+    		JOptionPane.showMessageDialog(null, "Mapa je zauzeta. Probajte malo kasnije.", "Sinkronizacija", JOptionPane.WARNING_MESSAGE);
+    		return;
+    	}
+  		importPanteonImpl(dir);
+  	} finally {
+  		sw.delete();
+  	}
+  }
+  
+  private static void importPanteonImpl(File dir) {
+  	lookupData ld = lookupData.getlookupData();
+  	
+  	File fiz = new File(dir, "Izme_nar.sdf");
+  	File fzag = new File(dir, "Osnova_n.sdf");
+  	File fst = new File(dir, "Postavke.sdf");
+  	
+  	if (!fiz.canRead()) {
+  		JOptionPane.showMessageDialog(null, "Nema novih narudžbi za import.", "Sinkronizacija", JOptionPane.WARNING_MESSAGE);
+  		return;
+  	}
+  	if (!fzag.canRead() || !fst.canRead()) {
+  		JOptionPane.showMessageDialog(null, "Greška kod sinkronizacije!", "Sinkronizacija", JOptionPane.ERROR_MESSAGE);
+  		return;
+  	}
+  	String line;
+  	List lzag = new ArrayList();
+  	List lst = new ArrayList();
+  	
+  	TextFile tzag = TextFile.read(fzag);
+  	if (tzag == null) {
+  		JOptionPane.showMessageDialog(null, "Greška kod sinkronizacije!", "Sinkronizacija", JOptionPane.ERROR_MESSAGE);
+  		return;
+  	}
+  	while (null != (line = tzag.in())) lzag.add(line);
+  	tzag.close();
+  	TextFile tst = TextFile.read(fst);
+  	if (tst == null) {
+  		JOptionPane.showMessageDialog(null, "Greška kod sinkronizacije!", "Sinkronizacija", JOptionPane.ERROR_MESSAGE);
+  		return;
+  	}
+  	while (null != (line = tst.in())) lst.add(line);
+  	tzag.close();
+  	
+  	QueryDataSet zag = doki.getDataModule().getTempSet(Condition.nil);
+    QueryDataSet st = stdoki.getDataModule().getTempSet(Condition.nil);
+    
+    zag.open();
+    st.open();
+    String oj = frmParam.getParam("robno", "ojPanteon", 
+        "01", "Org. jed. za narudžbe iz Panteona");
+    
+    String[] acc = {"CART", "CART1", "BC", "NAZART", "JM"};
+  	
+  	int zi = 0, si = 0;
+  	while (zi < lzag.size()) {
+  		line = (String) lzag.get(zi);
+  		
+  		zag.insertRow(false);
+      zag.setString("CSKL", oj);
+      zag.setString("VRDOK", "NKU");
+      zag.setInt("CPAR", Aus.getAnyNumber(line.substring(171, 206)));
+      zag.setTimestamp("DATDOK", getDate(line.substring(101, 136)));
+      zag.setTimestamp("DATDOSP", getDate(line.substring(136, 171)));
+      
+      zag.setString("GOD", hr.restart.util.Util.getUtil().
+          getYear(zag.getTimestamp("DATDOK")));
+      
+      String nbr = line.substring(49, 84);
+      
+      short rbr = 0;
+  		while (si < lst.size()) {
+  			line = (String) lst.get(si);
+  			if (!line.substring(49, 84).equals(nbr)) break;
+  			
+  			st.insertRow(false);
+        dM.copyColumns(zag, st, Util.mkey);
+        st.setShort("RBR", ++rbr);
+        st.setInt("RBSID", rbr);
+        
+        String cart = line.substring(166, 201).trim();
+        if (cart != null && ld.raLocate(dM.getDataModule().getArtikli(), "CART", cart)) {
+        	dM.copyColumns(dM.getDataModule().getArtikli(), st, acc);
+          st.post();
+        } else
+          throw new RuntimeException("Nepoznata šifra artikla!");
+        
+        st.setBigDecimal("KOL", Aus.getDecNumber(line.substring(271, 286)));
+        st.setBigDecimal("FC", Aus.getDecNumber(line.substring(324, 339)));
+        
+  			++si;
+  		}
+      
+  		saveOrder(zag, st);
+  			
+      ++zi;
+  	}
+  }
+  
+  private static Timestamp getDate(String sd) {
+  	Calendar cal = Calendar.getInstance();
+  	cal.set(cal.YEAR, Integer.parseInt(sd.substring(0, 4)));
+  	cal.set(cal.MONTH, Integer.parseInt(sd.substring(4, 6)) - 1);
+  	cal.set(cal.DAY_OF_MONTH, Integer.parseInt(sd.substring(6, 8)));
+  	if (sd.length() > 8) {
+  		cal.set(cal.HOUR_OF_DAY, Integer.parseInt(sd.substring(8, 10)));
+      cal.set(cal.MINUTE, Integer.parseInt(sd.substring(10, 12)));
+  	} else {
+  		cal.set(cal.HOUR_OF_DAY, 0);
+      cal.set(cal.MINUTE, 0);
+  	}
+  	cal.set(cal.SECOND, 0);
+  	cal.set(cal.MILLISECOND, 0);
+  	return new Timestamp(cal.getTime().getTime());
   }
 }
