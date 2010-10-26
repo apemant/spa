6d2c4b5d099989e624f55790ecb0d59b1bd9568d
abagaric <>
2010-10-26 18:49:20 +0000

    - rucni IPP

--- devel/projects/spa/src/hr/restart/pl/frmIPP.java
+++ devel/projects/spa/src/hr/restart/pl/frmIPP.java
@@ -2,21 +2,58 @@ package hr.restart.pl;
 
 import hr.restart.baza.dM;
 import hr.restart.sisfun.frmParam;
+import hr.restart.sisfun.frmTableDataView;
+import hr.restart.util.Aus;
 import hr.restart.util.Util;
 import hr.restart.util.Valid;
+import hr.restart.util.raImages;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import com.borland.dx.dataset.Column;
+import com.borland.dx.dataset.StorageDataSet;
 import com.borland.dx.dataset.Variant;
 import com.borland.dx.text.VariantFormatter;
 
 public class frmIPP extends frmSPL {
   HashMap datarow = new HashMap();
   private static frmIPP fIPP;
+  
+  frmTableDataView view = new frmTableDataView(true, false, true) {
+    protected void OKPress() {
+      hide();
+      convert();
+      ok_action_thread();
+      
+    }
+  };
+  
+  boolean manual;
+  JButton mbut = new JButton("Ruèni unos");
+  
   public frmIPP() {
     super();
     fIPP = this;
+    mbut.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        manualEntry();
+      }
+    });
+    JPanel down = new JPanel(new BorderLayout());
+    this.getContentPane().remove(okp);
+    down.add(mbut, BorderLayout.WEST);
+    down.add(okp);
+    view.okp.jBOK.setText("OK");
+    view.okp.jBOK.setIcon(raImages.getImageIcon(raImages.IMGOK));
+
+    this.getContentPane().add(down, BorderLayout.SOUTH);
   }
   
   public static frmIPP getFrmIPP() {
@@ -41,7 +78,87 @@ public class frmIPP extends frmSPL {
     System.out.println("formated "+formatter.format(v));
     return formatter.format(v);
   }
+  void manualEntry() {
+    if (!Validacija()) return;
+    okPress();
+    rcc.EnabDisabAllLater(getJPan(), true);
+    manual = true;
+    
+    StorageDataSet data = new StorageDataSet();
+    data.setColumns(new Column[] {
+        dM.createIntColumn("RBR", "Rbr"),
+        dM.createStringColumn("PRIM", "Vrsta primitka", 50),
+        dM.createIntColumn("BROJ", "Broj stjecatelja"),
+        dM.createBigDecimalColumn("OSN", "Osnovica", 2),
+        dM.createBigDecimalColumn("POR", "Porez", 2)
+    });
+    data.getColumn("BROJ").setTableName("dummy");
+    data.getColumn("OSN").setTableName("dummy");
+    data.getColumn("POR").setTableName("dummy");
+    data.open();
+    String[] prim = {"Plaæa", "Mirovina", "Drugi dohodak", "Dividende"};
+    String[][] cols = {
+        {"PLBROJ", "PLOSN", "PLPOR"},
+        {"MIRBROJ", "MIROSN", "MIRPOR"},
+        {"DDBROJ", "DDOSN", "DDPOR"},
+        {"DIVBROJ", "DIVOSN", "DIVPOR"}
+    };
+    for (int i = 0; i < 4; i++) {
+      data.insertRow(false);
+      data.setInt("RBR", i + 1);
+      data.setString("PRIM", prim[i]);
+      data.setUnassignedNull("BROJ");
+      data.setUnassignedNull("OSN");
+      data.setUnassignedNull("POR");
+      String br = (String) datarow.get(cols[i][0]);
+      String osn = (String) datarow.get(cols[i][1]);
+      String por = (String) datarow.get(cols[i][2]);
+      if (br != null && br.length() > 0)
+        data.setInt("BROJ", Aus.getNumber(br));
+      if (osn != null && osn.length() > 0)
+        data.setBigDecimal("OSN", Aus.getDecNumber(osn));
+      if (por != null && por.length() > 0)
+        data.setBigDecimal("POR", Aus.getDecNumber(por));
+    }
+    view.setDataSet(data);
+    view.setSums(new String[] {"BROJ", "OSN", "POR"});
+    view.show();
+    view.resizeLater();
+  }
+  
+  void convert() {
+    StorageDataSet data = view.jp.getStorageDataSet();
+
+    String[][] cols = {
+        {"PLBROJ", "PLOSN", "PLPOR"},
+        {"MIRBROJ", "MIROSN", "MIRPOR"},
+        {"DDBROJ", "DDOSN", "DDPOR"},
+        {"DIVBROJ", "DIVOSN", "DIVPOR"}
+    };
+    
+    for (data.first(); data.inBounds(); data.next()) {
+      if (!data.isNull("BROJ"))
+        datarow.put(cols[data.getInt("RBR")-1][0], 
+            Integer.toString(data.getInt("BROJ")));
+      if (!data.isNull("OSN"))
+        datarow.put(cols[data.getInt("RBR")-1][1], 
+            format(data.getBigDecimal("OSN")));
+      if (!data.isNull("POR"))
+        datarow.put(cols[data.getInt("RBR")-1][2], 
+            format(data.getBigDecimal("POR")));
+    }
+    datarow.put("UKUBROJ", view.jp.getSum("BROJ").toString());
+    datarow.put("UKUOSN", format(view.jp.getSum("OSN")));
+    datarow.put("UKUPOR", format(view.jp.getSum("POR")));
+  }
+  
+  public void afterReport() {
+    super.afterReport();
+    manual = false;
+  }
+  
   private void createReport() {
+    if (manual) return;
     raIniciranje.getInstance().posOrgsPl(fieldSet.getString("CORG"));
     datumispl = dm.getOrgpl().getTimestamp("DATUMISPL");
     short month = new Short(Util.getUtil().getMonth(datumispl)).shortValue();
