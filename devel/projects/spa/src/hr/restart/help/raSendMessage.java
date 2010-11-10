0b96e99d67be35e26571d85ae90cac6862ef0dab
abagaric <>
2010-11-10 11:48:26 +0000

    - dodan renderer

--- devel/projects/spa/src/hr/restart/help/raSendMessage.java
+++ devel/projects/spa/src/hr/restart/help/raSendMessage.java
@@ -1,29 +1,39 @@
 package hr.restart.help;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.plaf.basic.BasicComboBoxRenderer;
 
 import com.borland.dx.dataset.Column;
+import com.borland.dx.dataset.DataRow;
 import com.borland.dx.dataset.StorageDataSet;
 
 import hr.restart.baza.dM;
 import hr.restart.sisfun.raUser;
 import hr.restart.swing.JraScrollPane;
 import hr.restart.swing.raOptionDialog;
+import hr.restart.util.lookupData;
 import hr.restart.util.raComboBox;
+import hr.restart.util.raImages;
 
 
 public class raSendMessage extends raOptionDialog {
+  
+  lookupData ld = lookupData.getlookupData();
 
   JPanel pan = new JPanel(new BorderLayout());
   
@@ -75,6 +85,34 @@ public class raSendMessage extends raOptionDialog {
         });
       }
     });
+
+    dest.setRenderer(new BasicComboBoxRenderer() {
+      Font normal, bold;
+      public Component getListCellRendererComponent(JList list, Object value,
+          int index, boolean isSelected, boolean cellHasFocus) {
+        // TODO Auto-generated method stub
+        
+        JLabel comp = (JLabel) super.getListCellRendererComponent(
+            list, value, index, isSelected, cellHasFocus);
+        
+        if (list.getFont() != normal || bold == null)
+          bold = list.getFont().deriveFont(Font.BOLD);
+        normal = list.getFont();
+        
+        DataRow user = index < 0 ? null :
+          ld.raLookup(dM.getDataModule().getUseri(), "CUSER", dest.getDataValue(index));
+        if (user == null || !"D".equals(user.getString("LOKK"))) {
+          comp.setFont(normal);
+          comp.setIcon(null);
+        } else {
+          comp.setFont(bold);
+          comp.setIcon(raImages.getImageIcon(raImages.IMGTIP));
+        }
+        
+        return comp;
+      }
+    
+    });
   }
   
   public static void show(Container parent) {
@@ -83,6 +121,7 @@ public class raSendMessage extends raOptionDialog {
   
   private void showImpl(Container parent) {
     dest.removeAllItems();
+    dM.getDataModule().getUseri().refresh();
     dest.setRaItems(dM.getDataModule().getUseri(), "CUSER",  "NAZIV");
     dest.findCombo();
     msg.setText("");
