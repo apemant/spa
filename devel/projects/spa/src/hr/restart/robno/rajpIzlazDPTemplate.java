c817be44ec9a35e15fbb09ba5a4b629fa5000c94
abagaric <>
2012-04-27 15:08:54 +0000

    - popravak rinaba

--- devel/projects/spa/src/hr/restart/robno/rajpIzlazDPTemplate.java
+++ devel/projects/spa/src/hr/restart/robno/rajpIzlazDPTemplate.java
@@ -341,12 +341,13 @@ public class rajpIzlazDPTemplate extends JPanel {
 		 */
 		//		jrtbRezervacija.setSelected();
 		//		jrtbRezervacija.setText(isUserSelected()?"D":"N");
+	  boolean nd = raIzlazTemplate.isNabDirect();
 		JLabel rezervacija = new JLabel("Rezervacija");
 		rezervacija.setBorder(BorderFactory.createEtchedBorder());
 		rezervacija.setHorizontalAlignment(SwingConstants.CENTER);
 		rcc.setLabelLaF(rezervacija, false);
-		jpDetailCenter.add(rezervacija, new XYConstraints(500, 40, 108, -1));
-		jpDetailCenter.add(jrtbRezervacija, new XYConstraints(613, 40, 21, 21));
+		jpDetailCenter.add(rezervacija, new XYConstraints(500, nd ? 70: 40, 108, -1));
+		jpDetailCenter.add(jrtbRezervacija, new XYConstraints(613, nd ? 70 : 40, 21, 21));
 	}
 
 	public void setRezervacija() {
@@ -560,7 +561,7 @@ public class rajpIzlazDPTemplate extends JPanel {
 		jpDetailCenter.setLayout(xYLayoutDC);
 		xYLayoutDC.setHeight(50);
 		if (fDI.what_kind_of_dokument.equalsIgnoreCase("PON"))
-		  xYLayoutDC.setHeight(70);
+		  xYLayoutDC.setHeight(raIzlazTemplate.isNabDirect() ? 100 : 70);
 		if ((fDI.what_kind_of_dokument.equalsIgnoreCase("RAC") ||
 				fDI.what_kind_of_dokument.equalsIgnoreCase("ODB")) && raIzlazTemplate.isNabDirect())
 			xYLayoutDC.setHeight(80);
