3be88353e7e4d0d394cba30de363c27d7a0cb00f
and-iv <>
2012-05-08 13:39:11 +0000

    knjizenje blagajne kada ura uz stavku ima protustavke salda konti

--- devel/projects/spa/src/hr/restart/gk/frmKnjBlag.java
+++ devel/projects/spa/src/hr/restart/gk/frmKnjBlag.java
@@ -16,30 +16,135 @@
 **
 ****************************************************************************/
 package hr.restart.gk;
+import hr.restart.baza.Blagajna;
 import hr.restart.baza.Blagizv;
 import hr.restart.baza.Condition;
+import hr.restart.baza.dM;
+import hr.restart.blpn.frmBlag;
 import hr.restart.blpn.frmUplIspl;
+import hr.restart.sisfun.frmParam;
+import hr.restart.swing.JraButton;
+import hr.restart.swing.JraTextField;
 import hr.restart.util.Aus;
+import hr.restart.util.JlrNavField;
 import hr.restart.util.Util;
+import hr.restart.util.lookupData;
 import hr.restart.util.raTransaction;
 import hr.restart.zapod.OrgStr;
+import hr.restart.zapod.raKnjigChangeListener;
 import hr.restart.zapod.raKonta;
 
+import java.awt.BorderLayout;
 import java.math.BigDecimal;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.text.JTextComponent;
 
 import com.borland.dx.dataset.DataSet;
 import com.borland.dx.dataset.StorageDataSet;
 import com.borland.dx.sql.dataset.QueryDataSet;
+import com.borland.jbcl.layout.XYConstraints;
+import com.borland.jbcl.layout.XYLayout;
 public class frmKnjBlag extends frmKnjizenje {
   String domVal = hr.restart.zapod.Tecajevi.getDomOZNVAL();
   QueryDataSet izvjestaji;
+  
+  //*************
+  JPanel jpDetail = new JPanel();
+
+  XYLayout lay = new XYLayout();
+  JLabel jlCblag = new JLabel();
+  JLabel jlaCblag = new JLabel();
+  JLabel jlaNaziv = new JLabel();
+  JLabel jlaOznval = new JLabel();
+  JraButton jbSelCblag = new JraButton();
+  JlrNavField jlrCblag = new JlrNavField() {
+    public void after_lookUp() {
+    }
+  };
+  JlrNavField jlrOznval = new JlrNavField() {
+    public void after_lookUp() {
+    }
+  };
+  JlrNavField jlrNaziv = new JlrNavField() {
+    public void after_lookUp() {
+    }
+  };
+  StorageDataSet viewSet;
+  private StorageDataSet getSelDataSet() {
+    if (viewSet == null) {
+      viewSet = new StorageDataSet();
+      viewSet.addColumn(Blagajna.getDataModule().getColumn("CBLAG").cloneColumn());
+      viewSet.open();
+    }
+    return viewSet;
+  }
+  private void initblp() {
+    jpDetail.setLayout(lay);
+//    lay.setWidth(591);
+    lay.setHeight(50);
+
+    jlCblag.setText("Blagajna");
+    jlaCblag.setHorizontalAlignment(SwingConstants.CENTER);
+    jlaCblag.setText("Šifra");
+    jlaNaziv.setHorizontalAlignment(SwingConstants.CENTER);
+    jlaNaziv.setText("Naziv");
+    jlaOznval.setHorizontalAlignment(SwingConstants.CENTER);
+    jlaOznval.setText("Valuta");
+
+    jlrCblag.setColumnName("CBLAG");
+    jlrCblag.setDataSet(getSelDataSet());
+    jlrCblag.setColNames(new String[] {"OZNVAL", "NAZIV"});
+    jlrCblag.setTextFields(new JTextComponent[] {jlrOznval, jlrNaziv});
+    jlrCblag.setVisCols(new int[] {1, 2, 3}); /**@todo: Dodati visible cols za lookup frame */
+    jlrCblag.setSearchMode(0);
+    jlrCblag.setRaDataSet(frmBlag.getBlagajneKnjig());
+    OrgStr.getOrgStr().addKnjigChangeListener(new raKnjigChangeListener() {
+      public void knjigChanged(String oldKnjig, String newKnjig) {
+        jlrCblag.setRaDataSet(frmBlag.getBlagajneKnjig());
+        jlrOznval.setRaDataSet(frmBlag.getBlagajneKnjig());
+        jlrNaziv.setRaDataSet(frmBlag.getBlagajneKnjig());
+      }
+    });
+
+    jlrCblag.setNavButton(jbSelCblag);
+
+    jlrOznval.setColumnName("OZNVAL");
+    jlrOznval.setNavProperties(jlrCblag);
+    jlrOznval.setSearchMode(1);
+
+    jlrNaziv.setColumnName("NAZIV");
+    jlrNaziv.setNavProperties(jlrCblag);
+    jlrNaziv.setSearchMode(1);
+    
+    jpDetail.add(jbSelCblag, new XYConstraints(525, 20, 21, 21));
+    jpDetail.add(jlCblag, new XYConstraints(15, 20, -1, -1));
+    jpDetail.add(jlaCblag, new XYConstraints(151, 3, 48, -1));
+    jpDetail.add(jlaNaziv, new XYConstraints(256, 3, 265, -1));
+    jpDetail.add(jlaOznval, new XYConstraints(206, 3, 43, -1));
+    jpDetail.add(jlrCblag,  new XYConstraints(150, 20, 50, -1));
+    jpDetail.add(jlrNaziv,   new XYConstraints(255, 20, 265, -1));
+    jpDetail.add(jlrOznval,   new XYConstraints(205, 20, 45, -1));
+    jp.add(jpDetail,BorderLayout.CENTER);
+  }
+  //*************
+  
   public frmKnjBlag() {
+    initblp();
   }
   public boolean okPress() {
     if (!getKnjizenje().startKnjizenje(this)) return false;
-    izvjestaji = Blagizv.getDataModule().getFilteredDataSet(
-        Condition.whereAllEqual(new String[]{"STATUS","KNJIG"}, new String[] {"Z",OrgStr.getKNJCORG(false)})
-        .and(Condition.till("DATDO", dataSet.getTimestamp("DATUMDO"))));
+    Condition cond = Condition.whereAllEqual(new String[]{"STATUS","KNJIG"}, new String[] {"Z",OrgStr.getKNJCORG(false)})
+        .and(Condition.till("DATDO", dataSet.getTimestamp("DATUMDO")));
+    if (lookupData.getlookupData().raLocate(dM.getDataModule().getBlagajna(), "CBLAG", viewSet.getInt("CBLAG")+"")) {
+      System.out.println("BLAGAJNA " + viewSet.getInt("CBLAG") + " found!");
+      cond = cond.and(Condition.equal("CBLAG", viewSet.getInt("CBLAG")));
+    }
+    System.out.println(cond);
+    izvjestaji = Blagizv.getDataModule().getFilteredDataSet(cond);
 //      Util.getNewQueryDataSet("SELECT * FROM Blagizv WHERE status = 'Z' and knjig = '"+hr.restart.zapod.OrgStr.getKNJCORG(false)+
 //        "' and datdo < '"+new java.sql.Date(Util.getUtil().addDays(dataSet.getTimestamp("DATUMDO"),1).getTime()).toString()+"'");
     izvjestaji.open();
@@ -77,15 +182,15 @@ System.out.println("Usao u nenormalno");
                 stavka.setString("OPIS","Stavke -".concat(opisIzvj).concat(empty50).substring(0,50));
                 stavka.setTimestamp("DATDOK",izvjestaji.getTimestamp("DATDO"));
               } else {
-                stavka.setInt("CPAR", stavbl.getInt("CPAR"));
+                stavka.setInt("CPAR", uis.getInt("CPAR"));
                 stavka.setString("OPIS",stavbl.getString("OPIS").concat(opisIzvj).concat(empty50).substring(0,50));
                 stavka.setTimestamp("DATDOK",stavbl.getTimestamp("DATDOK"));
               }
               if (isSk(uis)) {
                 stavka.setTimestamp("DATDOSP", stavbl.getTimestamp("DATUM"));
                 stavka.setString("BROJDOK", uis.getString("BROJDOK"));
-                stavka.setString("VRDOK",frmNalozi.determineVrdok(uis, false));
-                stavka.setInt("CPAR", stavbl.getInt("CPAR"));
+                stavka.setString("VRDOK","NK!"/*frmNalozi.determineVrdok(uis, false)*/);
+                stavka.setInt("CPAR", uis.getInt("CPAR"));
               } 
               stavka.setInt("BROJIZV",izvjestaji.getShort("GODINA")*1000+izvjestaji.getInt("BRIZV"));
               if (!getKnjizenje().saveStavka()) return false;
@@ -142,7 +247,8 @@ System.out.println("Usao u nenormalno");
         }
         */
         String cstav = dev?"2":"1";
-        StorageDataSet promet = getKnjizenje().getNewStavka(getKnjizenje().getBrojKonta("BL","1",cstav),izvjestaji.getString("KNJIG"));
+        String cskl = frmParam.getParam("pl", "csklPromBL","1","Koja vrsta sheme sadrži konta za promet blagajne");
+        StorageDataSet promet = getKnjizenje().getNewStavka(getKnjizenje().getBrojKonta("BL",cskl,cstav),izvjestaji.getString("KNJIG"));
         getKnjizenje().setID(ukid);
         getKnjizenje().setIP(ukip);
         promet.setString("OPIS","Promet ".concat(opisIzvj).concat(empty50).substring(0,50));
