c2a46283dce935786cff558ace71264c6c0171ae
and-iv <>
2013-01-17 11:43:11 +0000

    IP xml v4.0 - zahtjeva novi ip-obrazac.jar (vidi \\aidell\tmp)

--- devel/projects/spa/src/hr/restart/pl/repIPDisk.java
+++ devel/projects/spa/src/hr/restart/pl/repIPDisk.java
@@ -88,8 +88,10 @@ public class repIPDisk extends repDisk {
     try {
       dumpHead();
       dumpDetail();
-      hr.porezna_uprava.e_porezna.obrasci.ip.v3_0.Generator.delimiter = separator;
-      new hr.porezna_uprava.e_porezna.obrasci.ip.v3_0.Generator("ip_head.csv", "ip_det.csv", new FileOutputStream(mxReport.TMPPRINTFILE));
+//      hr.porezna_uprava.e_porezna.obrasci.ip.v3_0.Generator.delimiter = separator;
+//      new hr.porezna_uprava.e_porezna.obrasci.ip.v3_0.Generator("ip_head.csv", "ip_det.csv", new FileOutputStream(mxReport.TMPPRINTFILE));
+      hr.porezna_uprava.e_porezna.obrasci.ip.v4_0.Generator.delimiter = separator;
+      new hr.porezna_uprava.e_porezna.obrasci.ip.v4_0.Generator("ip_head.csv", "ip_det.csv", new FileOutputStream(mxReport.TMPPRINTFILE));
     } catch (Exception e) {
       e.printStackTrace();
     }
