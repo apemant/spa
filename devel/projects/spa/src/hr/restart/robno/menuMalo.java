6244893385e4079da19e7c5940e6a81dea8860e9
abagaric <>
2013-03-29 14:40:43 +0000

    - automatika gotovinskih fisk

--- devel/projects/spa/src/hr/restart/robno/menuMalo.java
+++ devel/projects/spa/src/hr/restart/robno/menuMalo.java
@@ -17,13 +17,20 @@
 ****************************************************************************/
 package hr.restart.robno;
 
+import hr.restart.baza.Condition;
+import hr.restart.baza.Pos;
+import hr.restart.baza.doki;
+import hr.restart.pos.frmMasterBlagajna;
 import hr.restart.util.raLoader;
+import hr.restart.util.raProcess;
 
 import java.awt.event.ActionEvent;
 import java.util.ResourceBundle;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+
+import com.borland.dx.dataset.DataSet;
 
 /**
  * <p>Title: </p>
@@ -110,14 +117,39 @@ public class menuMalo extends JMenu {
     hr.restart.robno.raPRDkup raprd = (hr.restart.robno.raPRDkup)raLoader.load("hr.restart.robno.raPRDkup");
     hr.restart.robno.presPRDkup.getPres().showJpSelectDoc("PRD", raprd, true, jmPRD.getText());
   }
+  
+  
   void jmGOT_actionPerformed(ActionEvent e) {
+    fiskRed();
+    
     hr.restart.robno.raGOT ragot = (hr.restart.robno.raGOT)raLoader.load("hr.restart.robno.raGOT");
     hr.restart.robno.presGOT.getPres().showJpSelectDoc("GOT", ragot, true, jmGOT.getText());
   }
   void jmGRN_actionPerformed(ActionEvent e) {
+    fiskRed();
+    
     hr.restart.robno.raGRN ragrn = (hr.restart.robno.raGRN)raLoader.load("hr.restart.robno.raGRN");
     hr.restart.robno.presGRN.getPres().showJpSelectDoc("GRN", ragrn, true, jmGRN.getText());
   }
+  
+  long lastTrans = 0;
+  void fiskRed() {
+    if (lastTrans == 0 || System.currentTimeMillis() > lastTrans + 1000 * 60 * 60 * 8)
+      raProcess.runChild(SF, "Fiskalizacija", "Fiskalizacija nepotpuno zakljuèenih raèuna...", new Runnable() {    
+        public void run() {
+          lastTrans = System.currentTimeMillis();
+          DataSet ms = doki.getDataModule().getTempSet("fok='D' AND (jir='' or jir is NULL) AND " +
+          		"(vrdok in ('GRN','GOT') OR (vrdok='PRD' AND param='K'))");
+          ms.open();
+          System.out.println("Fiskalizacija " + ms.rowCount() + " crvenih raèuna...");
+          if (ms.rowCount() == 0) return;
+          for (ms.first(); ms.inBounds(); ms.next()) {
+            raIzlazTemplate.fisk(ms);
+          }
+        }
+      });
+  }
+  
   void jmOTP_actionPerformed(ActionEvent e) {
     hr.restart.robno.raOTP ragot = (hr.restart.robno.raOTP)raLoader.load("hr.restart.robno.raOTP");
     hr.restart.robno.presOTP.getPres().showJpSelectDoc("OTP", ragot, true, jmOTP.getText());
