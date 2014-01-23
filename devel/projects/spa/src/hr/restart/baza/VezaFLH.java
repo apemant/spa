2cf8bcb281d92464e94e185de0faa9332f0ec940
abagaric <>
2014-01-23 20:18:50 +0000

    - za FLH

---
+++ devel/projects/spa/src/hr/restart/baza/VezaFLH.java
@@ -0,0 +1,48 @@
+/****license*****************************************************************
+**   file: VezaFHL.java
+**   Copyright 2013 Rest Art
+**
+**   Licensed under the Apache License, Version 2.0 (the "License");
+**   you may not use this file except in compliance with the License.
+**   You may obtain a copy of the License at
+**
+**       http://www.apache.org/licenses/LICENSE-2.0
+**
+**   Unless required by applicable law or agreed to in writing, software
+**   distributed under the License is distributed on an "AS IS" BASIS,
+**   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**   See the License for the specific language governing permissions and
+**   limitations under the License.
+**
+****************************************************************************/
+package hr.restart.baza;
+
+import com.borland.dx.sql.dataset.QueryDataSet;
+
+public class VezaFLH extends KreirDrop {
+
+  private static VezaFLH vezaflhclass;
+  
+  QueryDataSet vezaflh = new QueryDataSet();
+  
+  public static VezaFLH getDataModule() {
+    if (vezaflhclass == null) {
+      vezaflhclass = new VezaFLH();
+    }
+    return vezaflhclass;
+  }
+
+  public QueryDataSet getQueryDataSet() {
+    return vezaflh;
+  }
+
+  public VezaFLH() {
+    try {
+      modules.put(this.getClass().getName(), this);
+      initModule();
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
