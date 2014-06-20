3fe1dcd23ffd3dd951c748c0ddfe3fb64668d0ba
abagaric <>
2014-06-20 13:14:28 +0000

    - klasa za heuristicku provjeru imena klijenata

--- devel/projects/spa/src/hr/restart/crm/raKlijentNames.java
+++ devel/projects/spa/src/hr/restart/crm/raKlijentNames.java
@@ -1,16 +1,134 @@
 package hr.restart.crm;
 
-import java.util.ArrayList;
-import java.util.List;
+import hr.restart.baza.Klijenti;
+import hr.restart.baza.dM;
+import hr.restart.sisfun.frmParam;
+import hr.restart.util.Aus;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import com.borland.dx.dataset.Column;
+import com.borland.dx.dataset.DataSet;
+import com.borland.dx.dataset.StorageDataSet;
 
 
 public class raKlijentNames {
-
-  List clients = new ArrayList();
   
-  public raKlijentNames() {
-    // TODO Auto-generated constructor stub
+  private static raKlijentNames inst = new raKlijentNames();
+
+  private Set clients = new HashSet();
+  private int serial = -1;
+  private double thresh = 0.8;
+  private StorageDataSet result;
+  
+  private raKlijentNames() {
+    checkThreshold();
+    checkChanges();
+    result = new StorageDataSet();
+    result.setColumns(new Column[] {
+       Klijenti.getDataModule().getColumn("CKLIJENT").cloneColumn(),
+       Klijenti.getDataModule().getColumn("NAZIV").cloneColumn(),
+       Klijenti.getDataModule().getColumn("MJ").cloneColumn(),
+       Klijenti.getDataModule().getColumn("OIB").cloneColumn(),
+       dM.createStringColumn("RAZLOG", "Razlog", 50)
+    });
+    result.open();
   }
-
   
+  public static raKlijentNames getInstance() {
+    return inst;
+  }
+  
+  private boolean checkThreshold() {
+    thresh = Aus.getDecNumber(frmParam.getParam("crm", "heurGranica", "0.8", 
+        "Granica provjere sliènosti postojeæih klijenata")).doubleValue();
+    if (thresh > 0.99) thresh = 0.99;
+    if (thresh < 0.5 && thresh > 0) thresh = 0.5;
+    return thresh > 0;
+  }
+  
+  public void checkChanges() {
+    if (!checkThreshold()) return;
+    
+    int newSerial = dM.getDataModule().getSynchronizer().getSerialNumber("klijenti");
+    if (serial == newSerial) return;
+    serial = newSerial;
+    clients.clear();
+    DataSet ds = Klijenti.getDataModule().getQueryDataSet();
+    ds.open();
+    for (ds.first(); ds.inBounds(); ds.next())
+      clients.add(new ClientData(ds));
+  }
+  
+  public void addRow(DataSet ds) {
+    if (!checkThreshold()) return;
+    
+    serial = dM.getDataModule().getSynchronizer().getSerialNumber("klijenti");
+    clients.add(new ClientData(ds));
+  }
+  
+  public void removeRow(int cklijent) {
+    if (!checkThreshold()) return;
+    
+    serial = dM.getDataModule().getSynchronizer().getSerialNumber("klijenti");
+    clients.remove(new ClientData(cklijent));
+  }
+  
+  
+  public DataSet findSimilar(DataSet ds) {
+    if (!checkThreshold()) return null;
+    
+    double sim;
+    result.empty();
+    for (Iterator i = clients.iterator(); i.hasNext(); ) {
+      ClientData cd = (ClientData) i.next();
+      if (cd.cklijent == ds.getInt("CKLIJENT")) continue;
+      if (ds.getString("OIB").trim().length() > 0 && cd.oib.length() > 0 && 
+          cd.oib.equals(ds.getString("OIB").trim())) cd.addTo(result, "Identièan OIB");
+      else if (ds.getString("OIB").trim().length() == 0 || cd.oib.length() == 0)
+        if ((sim = Aus.heuristicCompare(ds.getString("NAZIV"), cd.naziv)) >= thresh)
+          cd.addTo(result, "Slièan naziv (faktor " + Aus.formatFloat((float) sim) + ")");
+    }
+    return result;
+  }
+  
+  
+  public static class ClientData {
+    int cklijent;
+    String naziv;
+    String mj;
+    String oib;
+    
+    public ClientData(int cklijent) {
+      this.cklijent = cklijent;
+    }
+    
+    public ClientData(DataSet ds) {
+      cklijent = ds.getInt("CKLIJENT");
+      naziv = ds.getString("NAZIV");
+      mj = ds.getString("MJ");
+      oib = ds.getString("OIB").trim();
+    }
+    
+    public void addTo(DataSet ds, String razlog) {
+      ds.insertRow(false);
+      ds.setInt("CKLIJENT", cklijent);
+      ds.setString("NAZIV", naziv);
+      ds.setString("MJ", mj);
+      ds.setString("OIB", oib);
+      ds.setString("RAZLOG", razlog);
+    }
+    
+    public int hashCode() {
+      return cklijent;
+    }
+    
+    public boolean equals(Object obj) {
+      if (obj instanceof ClientData) 
+        return ((ClientData) obj).cklijent == this.cklijent;
+      return false;
+    }
+  }
 }
