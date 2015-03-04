87f06549c31bc2d25d7d34fcf3e9b2c856f6e0a7
abagaric <>
2015-03-04 20:17:59 +0000

    - dodatne metode, key prebacen na Object

--- devel/projects/spa/src/hr/restart/util/HashSum.java
+++ devel/projects/spa/src/hr/restart/util/HashSum.java
@@ -5,14 +5,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 import com.borland.dx.dataset.DataSet;
+import com.borland.dx.dataset.ReadRow;
 import com.borland.dx.dataset.Variant;
 
 
 public class HashSum {
   HashMap map = new HashMap();
   DataSet ds;
-  Variant v = new Variant();
   String keyCol, valCol;
+  int keyType, valType;
 
   public HashSum() {
     //
@@ -22,48 +23,107 @@ public class HashSum {
     this.ds = ds;
     this.keyCol = keyCol;
     this.valCol = valCol;
+    keyType = ds.getColumn(keyCol).getDataType();
+    valType = ds.getColumn(valCol).getDataType();
   }
 
+  public void set(DataSet ds, String keyCol, String valCol) {
+    this.ds = ds;
+    this.keyCol = keyCol;
+    this.valCol = valCol;
+  }
+  
+  public void set(DataSet ds, String valCol) {
+    this.ds = ds;
+    this.valCol = valCol;
+  }
+  
+  public void set(DataSet ds) {
+    this.ds = ds;
+  }
+  
+  public void set(String valCol) {
+    this.valCol = valCol;
+  }
+  
+  private Object getKey(ReadRow row) {
+    if (keyType == Variant.STRING)
+      return row.getString(keyCol);
+    if (keyType == Variant.INT)
+      return Integer.valueOf(row.getInt(keyCol));
+    if (keyType == Variant.SHORT)
+      return Integer.valueOf(row.getShort(keyCol));
+    return null;
+  }
+  
+  public void add(ReadRow row) {
+    add(getKey(row), valCol);
+  }
+  
   public void add() {
-    ds.getVariant(keyCol, ds.getRow(), v);
-    add(v.toString());
+    add(getKey(ds), valCol);
   }
   
-  private void add(String key) {
-    ds.getVariant(valCol, ds.getRow(), v);
-    if (v.getType() == Variant.BIGDECIMAL)
-      add(key, v.getBigDecimal());
-    else if (v.getType() == Variant.DOUBLE)
-      add(key, v.getDouble());
-    else if (v.getType() == Variant.FLOAT)
-      add(key, v.getAsDouble());
+  public void addVal(String valc) {
+    add(getKey(ds), valc);
   }
   
-  public void add(String key, BigDecimal val) {
+  public BigDecimal get() {
+    return get(getKey(ds));
+  }
+  
+  public BigDecimal get(ReadRow row) {
+    return get(getKey(row));
+  }
+  
+  private void add(Object key, String valc) {
+    if (valType == Variant.BIGDECIMAL)
+      add(key, ds.getBigDecimal(valc));
+    else if (valType == Variant.DOUBLE)
+      add(key, ds.getDouble(valc));
+    else if (valType == Variant.FLOAT)
+      add(key, ds.getFloat(valc));
+  }
+  
+  public void add(Object key, BigDecimal val) {
     BigDecimal old = (BigDecimal) map.get(key);
     if (old == null) map.put(key, val);
     else map.put(key, val == null ? old : val.add(old));
   }
   
-  public void add(String key, double val) {
+  public void add(Object key, double val) {
     Double old = (Double) map.get(key);
     if (old == null) map.put(key, new Double(val));
     else map.put(key, new Double(val + old.doubleValue()));
   }
   
-  public BigDecimal get(String key) {
+  public BigDecimal get(Object key) {
     return (BigDecimal) map.get(key);
   }
   
-  public BigDecimal getBigDecimal(String key) {
+  public BigDecimal getBigDecimal(Object key) {
     return (BigDecimal) map.get(key);
   }
   
-  public double getDouble(String key) {
+  public double getDouble(Object key) {
     return ((Double) map.get(key)).doubleValue();
   }
   
   public Iterator iterator() {
     return map.keySet().iterator();
+  }
+  
+  public BigDecimal total() {
+    BigDecimal tot = Aus.zero0;
+    for (Iterator i = map.values().iterator(); i.hasNext(); )
+      tot = tot.add((BigDecimal) i.next());
+    return tot;
+  }
+  
+  public double totalDouble() {
+    double tot = 0;
+    for (Iterator i = map.values().iterator(); i.hasNext(); )
+      tot += ((Double) i.next()).doubleValue();
+    return tot;
   }
 }
