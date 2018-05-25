3acdd190f17c83a3fe8a404c5b0a6963f78e7b94
ante <>
2018-05-25 08:56:14 +0000

    - broj decimala

--- devel/projects/spa/src/hr/restart/baza/Cjenik.java
+++ devel/projects/spa/src/hr/restart/baza/Cjenik.java
@@ -17,6 +17,10 @@
 ****************************************************************************/
 package hr.restart.baza;
 
+import hr.restart.sisfun.frmParam;
+import hr.restart.util.Aus;
+
+import com.borland.dx.dataset.Column;
 import com.borland.dx.dataset.DataModule;
 
 
@@ -30,5 +34,17 @@ public class Cjenik extends KreirDrop implements DataModule {
 
   public boolean isAutoRefresh() {
     return true;
+  }
+  
+  protected void modifyColumn(Column c) {
+    if (c.getColumnName().equals("VCKALDOM") || c.getColumnName().equals("VCKALVAL") ||
+        c.getColumnName().equals("VC") || c.getColumnName().equals("MC")) {
+      int scale = Aus.getNumber(frmParam.getParam("robno", "cijenaDec", 
+          "2", "Broj decimala za cijenu na izlazu (2-4)").trim());
+      c.setScale(scale);
+      c.setPrecision(c.getPrecision() - 2 + scale);
+      if (scale > 0 && scale < 8)
+        c.setDisplayMask("###,###,##0."+hr.restart.util.Aus.string(scale, '0'));
+    }
   }
 }
