77267f140e3be7df66c0ef20e3f43c7f49f63e9a
ante <>
2018-05-25 09:10:58 +0000

    - veza za komisiju (todo)

---
+++ devel/projects/spa/src/hr/restart/baza/VezaKom.java
@@ -0,0 +1,28 @@
+/****license*****************************************************************
+**   file: VezaKom.java
+**   Copyright 2013 Rest Art
+**
+**   Licensed under the Apache License, Version 2.0 (the "License");
+**   you may not use this file except in compliance with the License.
+**   You may obtain a copy of the License at
+**
+**       http://www.apache.org/licenses/LICENSE-2.0
+**
+**   Unless required by applicable law or agreed to in writing, software
+**   distributed under the License is distributed on an "AS IS" BASIS,
+**   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**   See the License for the specific language governing permissions and
+**   limitations under the License.
+**
+****************************************************************************/
+package hr.restart.baza;
+
+public class VezaKom extends KreirDrop {
+
+  private static VezaKom inst = new VezaKom();
+  
+  public static VezaKom getDataModule() {
+    return inst;
+  }
+  
+}
