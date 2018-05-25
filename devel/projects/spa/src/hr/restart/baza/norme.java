a80b8d2655fe18e507ddab68940524f9b84a2255
ante <>
2018-05-25 09:09:36 +0000

    - hashdataset support

--- devel/projects/spa/src/hr/restart/baza/norme.java
+++ devel/projects/spa/src/hr/restart/baza/norme.java
@@ -18,31 +18,101 @@
 package hr.restart.baza;
 
 import hr.restart.util.Aus;
+import hr.restart.util.HashDataSet;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.borland.dx.dataset.DataModule;
+import com.borland.dx.dataset.ReadRow;
 import com.borland.dx.sql.dataset.QueryDataSet;
 
 public class norme extends KreirDrop implements DataModule {
   
 	private static norme inst = new norme();
+	
+	private Map normap;
+	
+	int dsSerial = -1;
+	
+	HashDataSet aktivhash;
   
-  QueryDataSet normesorted = new raDataSet();
+  /*QueryDataSet normesorted = new raDataSet();
   
   {
   	createFilteredDataSet(normesorted, "");
   	Aus.setFilter(normesorted, "SELECT * FROM norme ORDER BY cartnor");
     //normesorted.setSort(new SortDescriptor(new String[] {"CARTNOR"}));
-  }
+  	
+  }*/
   
   public static norme getDataModule() {
     return inst;
   }
 
-  public com.borland.dx.sql.dataset.QueryDataSet getSorted() {
+/*  public com.borland.dx.sql.dataset.QueryDataSet getSorted() {
     return normesorted;
+  }*/
+  
+  private static void checkHash() {
+    int now = dM.getSynchronizer().getSerialNumber("NORME");
+    if (now != inst.dsSerial || inst.aktivhash == null) {
+      inst.dsSerial = now;
+      inst.normap = new HashMap();
+      QueryDataSet ds = inst.getQueryDataSet();
+      ds.open();
+      for (ds.first(); ds.inBounds(); ds.next()) {
+        Integer key = Integer.valueOf(ds.getInt("CARTNOR"));
+        List rows = (List) inst.normap.get(key);
+        if (rows == null) inst.normap.put(key, rows = new ArrayList());
+        rows.add(new Row(ds));
+      }
+      inst.aktivhash = new HashDataSet(inst.getQueryDataSet(), new String[] {"CARTNOR", "CART"});
+    }
+  }
+  
+  public static boolean check(int norm) {
+    checkHash();
+    return inst.normap.containsKey(Integer.valueOf(norm));
+  }
+  
+  public static int count(int norm) {
+    if (!check(norm)) return 0;
+    return ((List) inst.normap.get(Integer.valueOf(norm))).size();
+  }
+  
+  public static BigDecimal kol(int norm, int rbr) {
+    if (!check(norm)) return BigDecimal.ZERO;
+    return ((Row) ((List) inst.normap.get(Integer.valueOf(norm))).get(rbr)).kol;
+  }
+  
+  public static int cart(int norm, int rbr) {
+    if (!check(norm)) return -1;
+    List rows = (List) inst.normap.get(Integer.valueOf(norm));
+    if (rbr < 0 || rbr >= rows.size()) return -1;
+    return ((Row) rows.get(rbr)).cart;
+  }
+  
+  public static ReadRow art(int norm, int rbr) {
+    int cart = cart(norm, rbr);
+    if (cart < 0 || !inst.aktivhash.loc(new String[] {Integer.toString(norm), Integer.toString(cart)})) return null;
+    return inst.aktivhash.get();
   }
 
   public boolean isAutoRefresh() {
     return true;
+  }
+  
+  public static class Row {
+    int cart;
+    BigDecimal kol;
+    
+    public Row(ReadRow ds) {
+      cart = ds.getInt("CART");
+      kol = ds.getBigDecimal("KOL");
+    }
   }
 }
